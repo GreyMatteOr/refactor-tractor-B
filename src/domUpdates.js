06b2b67b2fa2e929d00207d0e2e07aa5dfebd3da
Matthew Lane <themplane@gmail.com>
2020-09-07 22:01:14 -0700

    Separate dom nodes and dom methods into 2 new files.

---
+++ src/domUpdates.js
@@ -0,0 +1,133 @@
+let domUpdates = {
+  greetUser(firstName) {
+    let welcomeMsg = `
+      <div class="welcome-msg">
+        <h1>Welcome ${firstName}!</h1>
+      </div>`;
+    document.querySelector(".banner-image").insertAdjacentHTML("afterbegin", welcomeMsg);
+  },
+
+  addToDom(recipeInfo, shortRecipeName, node) {
+    let cardHtml = `
+      <div class="recipe-card" id=${recipeInfo.id}>
+        <h3 maxlength="40">${shortRecipeName}</h3>
+        <div class="card-photo-container">
+          <img src=${recipeInfo.image} class="card-photo-preview" alt="${recipeInfo.name} recipe" title="${recipeInfo.name} recipe">
+          <div class="text">
+            <div>Click for Instructions</div>
+          </div>
+        </div>
+        <h4>${recipeInfo.tags[0]}</h4>
+        <img src="./images/apple-logo-outline.png" alt="unfilled apple icon" class="card-apple-icon">
+      </div>`
+    node.insertAdjacentHTML("beforeend", cardHtml);
+  },
+
+  listTags(allTags, node) {
+    allTags.forEach(tag => {
+      let tagHtml = `<li><input type="checkbox" class="checked-tag" id="${tag}">
+        <label for="${tag}">${this.capitalize(tag)}</label></li>`;
+      node.insertAdjacentHTML("beforeend", tagHtml);
+    });
+  },
+
+  capitalize(words) {
+    return words.split(" ").map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  },
+
+  hideRecipes(recipes) {
+    recipes.forEach(recipe => {
+      let domRecipe = document.getElementById(`${recipe.id}`);
+      domRecipe.style.display = "none";
+    });
+  },
+
+  updatePicture(node, img) {
+    node.src = img;
+  },
+
+  makeInline(node) {
+    node.style.display = "inline";
+  },
+
+  addOverlay(node) {
+    node.insertAdjacentHTML("beforebegin", "<section id='overlay'></div>");
+  },
+
+  generateRecipeTitle(recipe, ingredients, node) {
+    let recipeTitle = `
+      <button id="exit-recipe-btn">X</button>
+      <h3 id="recipe-title">${recipe.name}</h3>
+      <h4>Ingredients</h4>
+      <p>${ingredients}</p>`
+    node.insertAdjacentHTML("beforeend", recipeTitle);
+  },
+
+  addRecipeImage(recipe) {
+    document.getElementById("recipe-title").style.backgroundImage = `url(${recipe.image})`;
+  },
+
+  generateInstructions(recipe, node) {
+    let instructionsList = "";
+    let instructions = recipe.instructions.map(i => {
+      return i.instruction
+    });
+    instructions.forEach(i => {
+      instructionsList += `<li>${i}</li>`
+    });
+    node.insertAdjacentHTML("beforeend", "<h4>Instructions</h4>");
+    node.insertAdjacentHTML("beforeend", `<ol>${instructionsList}</ol>`);
+  },
+
+  exitRecipe(node) {
+    while (node.firstChild && node.removeChild(node.firstChild));
+    node.style.display = "none";
+    document.getElementById("overlay").remove();
+  },
+
+  showMyRecipesBanner() {
+    document.querySelector(".welcome-msg").style.display = "none";
+    document.querySelector(".my-recipes-banner").style.display = "block";
+  },
+
+  showWelcomeBanner() {
+    document.querySelector(".welcome-msg").style.display = "flex";
+    document.querySelector(".my-recipes-banner").style.display = "none";
+  },
+
+  showAllRecipes(recipes) {
+    recipes.forEach(recipe => {
+      let domRecipe = document.getElementById(`${recipe.id}`);
+      domRecipe.style.display = "block";
+    });
+    domUpdates.showWelcomeBanner();
+  },
+
+  toggleMenu(menuOpen) {
+    var menuDropdown = document.querySelector(".drop-menu");
+    if (!menuOpen) {
+      menuDropdown.style.display = "block";
+    } else {
+      menuDropdown.style.display = "none";
+    }
+    return !menuOpen;
+  },
+
+  displayPantryInfo(pantry) {
+    pantry.forEach(ingredient => {
+      let ingredientHtml = `<li><input type="checkbox" class="pantry-checkbox" id="${ingredient.name}">
+        <label for="${ingredient.name}">${ingredient.name}, ${ingredient.count}</label></li>`;
+      document.querySelector(".pantry-list").insertAdjacentHTML("beforeend", ingredientHtml);
+    });
+  },
+
+  hideRecipe(id) {
+    let domRecipe = document.getElementById(`${id}`);
+    domRecipe.style.display = "none";
+  }
+
+};
+
+export default domUpdates;
