cc2884e78c0b9653089dc3e26e4b414ed8f02f00
Matthew Lane <themplane@gmail.com>
2020-09-15 09:56:53 -0700

    Add New down arrown icon in mobile and re-display the tag search aside

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,6 +2,7 @@ let domUpdates = {
   greetUser(firstName) {
     let welcomeMsg = `
       <div class="welcome-msg">
+        <img id='tag-toggle' alt='A toggle arrow that shows and hides the search-by-tag menu' src='./images/down-arrow.png'/>
         <h1>Welcome ${firstName}!</h1>
       </div>`;
     document.querySelector(".banner-image").insertAdjacentHTML("afterbegin", welcomeMsg);
